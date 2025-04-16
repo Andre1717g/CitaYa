78285e25d8f232c19a9f3d89dcc745102209354f
Andre1717g <vv18009@ues.edu.sv>
2025-04-15 19:30:29 -0600

    nuevos campos

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -16,6 +16,12 @@
                 <li class="nav-item">
                     <a class="nav-link" href="#">Citas</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Iniciar Sesion</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Registrar</a>
+                </li>
             </ul>
         </div>
     </div>
